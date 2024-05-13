a4dd8e3f1d4972975c7b3165c0953db1c5f157d2
Trần Công Quân <tcquan.it@gmail.com>
2024-05-13 08:59:18 +0700

    feature/add-author-admin-crud: add author admin crud

---
+++ resources/views/vendor/backpack/ui/inc/styles.blade.php
@@ -0,0 +1,25 @@
+@basset('https://cdnjs.cloudflare.com/ajax/libs/noty/3.1.4/noty.min.css')
+@basset('https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css')
+@basset('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css')
+
+@basset(base_path('vendor/backpack/crud/src/resources/assets/css/common.css'))
+
+@if (backpack_theme_config('styles') && count(backpack_theme_config('styles')))
+    @foreach (backpack_theme_config('styles') as $path)
+        @if(is_array($path))
+            @basset(...$path)
+        @else
+            @basset($path)
+        @endif
+    @endforeach
+@endif
+
+@if (backpack_theme_config('mix_styles') && count(backpack_theme_config('mix_styles')))
+    @foreach (backpack_theme_config('mix_styles') as $path => $manifest)
+        <link rel="stylesheet" type="text/css" href="{{ mix($path, $manifest) }}">
+    @endforeach
+@endif
+
+@if (backpack_theme_config('vite_styles') && count(backpack_theme_config('vite_styles')))
+    @vite(backpack_theme_config('vite_styles'))
+@endif
